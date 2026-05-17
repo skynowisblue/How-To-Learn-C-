3af478e9fe64095e4ec8268d9437e515d266ba2b
skynowisblue <vadikalachiranjeevi@gmail.com>
2026-05-17 21:42:12 +0530

    Lesson 2:Int Var's

---
+++ IntVar's.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  //In The Before Lesson We Made Hello, World Congrats Now We Will Make Var's Short For variables
+  //Now We Learn About Ints Short for Integers Intergers Are Just Whole Numbers
+  int x=90;//unlike some languages C Is Strong Typed So It Needs You To Specifie The type then Var Name then Value
+  printf("%d",x);//this prints 90 Why? x contains 90 and Whats that %d Its A Format Specifier C Uses It For Ints
+                 //so in a nutshell this prints x value
+  return 0;//dont forget return 0;
+}
