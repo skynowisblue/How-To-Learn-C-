63f824cfdb4920e42f5f734711b344079c021d0e
skynowisblue <vadikalachiranjeevi@gmail.com>
2026-05-17 22:05:44 +0530

    Lesson 6:ChangeVar[Important].c

---
+++ ChangeVar's.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+   //Last lesson We Did Not Did That Much So We Will Be Learning About Changing Vars
+   int x=90;
+   printf("Before X:%d",x);
+   x=23;
+   printf("After X:%d",x);
+   return 0;
+}
