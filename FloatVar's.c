71150f251bde578993424727adcac7fc92be2003
skynowisblue <vadikalachiranjeevi@gmail.com>
2026-05-17 21:50:42 +0530

    Lesson 3: FloatVar's.c

---
+++ FloatVar's.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  //Before Lesson We Learned About Ints Which Take Up 4 bytes of space per 1 int 
+  //So We'll Learn about Floats
+  //float Are For floating point numbers like 0.1,0.3 Floats Take Up 4 bytes Too
+  //for printing Floats Use %f
+  float x=0.1;//type name = value Easy!
+  printf("%f",x);
+  return 0;
+}
--- OtherVar's.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(){
-  //Before Lesson We Learned About Ints 
-  //So We'll Learn about More Types Of Ints 
-  //float 
-}
