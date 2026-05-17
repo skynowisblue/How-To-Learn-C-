ffe7c46799b7c574958ffd417b6ada10a3600057
skynowisblue <vadikalachiranjeevi@gmail.com>
2026-05-17 21:33:40 +0530

    First Lesson: How To Make Hello World in C

---
+++ HelloWorld.c
@@ -0,0 +1,5 @@
+#include<stdio.h>//#include Which Includes Stdio.h or any lib Stdio.h is a standard lib for I/O in C And It Has Functions like printf,scanf and many more
+int main(){//This is the start of the file
+  printf("Hello, World");//prints Hello, World
+  return 0;//this Tell's The OS that the Program Ran Well Without Error's
+}
