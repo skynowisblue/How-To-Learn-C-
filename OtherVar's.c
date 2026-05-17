3ff7d58144bd101a68aa1565644eb75b9f52ab2b
skynowisblue <vadikalachiranjeevi@gmail.com>
2026-05-17 21:59:44 +0530

    Lesson 5:OtherVar's.c

--- OtherVar's.c
+++ OtherVar's.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int main(){
+  //We'll Learn About Doubles,char And \n In This Lesson
+  //Doubles Are Just Floats With More Precision 
+  //1 Double takes up 8 bytes
+  //chars on the other hand are just 1 letter var
+  //1 char takes 1 byte
+  //Double "%lf",Char "%c"
+  //\n in c is For Newline 
+  double x=1.22741;
+  char Alphabet='A';
+  printf("%lf\n",x);
+  printf("%c",Alphabet);
+}
